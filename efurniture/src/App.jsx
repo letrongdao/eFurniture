786a50b4001bdd9112936a431339e9d2bb662cd9
DANG NGUYEN <noplstks@gmail.com>
2024-01-05 14:06:07 +0700

    HomePage Template updated

--- efurniture/src/App.jsx
+++ efurniture/src/App.jsx
@@ -1,16 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>First test</h1>
+      {/* <h1>First test</h1> */}
+      <div className="App" style={{ overflowX: "hidden" }}>
+        
+        {/* <Router>
+          <Navbar />
+          <Switch>
+            <Route to="/" component={Home} />
+          </Switch>
+        </Router> */}
+        <Navbar />
+        <Home />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
