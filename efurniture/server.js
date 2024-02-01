4f12352062339edb1ae7e27f1ef63c1e86a8afc5
thanhdong06 <dong82c@gmail.com>
2024-02-01 17:00:07 +0700

    feat: API Product

--- efurniture/server.js
+++ efurniture/server.js
@@ -38,14 +38,6 @@ app.get('/users/:id', (req, res) => {
   })
 })
 
-app.get('/products', (req, res) => {
-  const sql = "SELECT * FROM products"
-  db.query(sql, (err, result) => {
-    if (err) res.json(err)
-    return res.json(result)
-  })
-})
-
 app.post('/register', async (req, res) => {
   const sql = "INSERT INTO users (user_id, email, password, fullName, role_id, phone, create_at, status) VALUES (?,?,?,?,?,?,?,?)"
   const values = [req.body.userId, req.body.email, req.body.password, req.body.fullName, req.body.roleId, req.body.phone, req.body.createAt, req.body.status]
@@ -57,6 +49,78 @@ app.post('/register', async (req, res) => {
       console.log("Successfully registered")
     }
   })
+})
+
+app.get('/products', (req, res) => {
+  const sql = "SELECT * FROM products";
+  db.query(sql, (err, result) => {
+    if (err) res.json(err)
+    return res.json(result)
+  })
+})
+
+app.get('/products', (req, res) => {
+  const productId = [req.query.product_id];
+  const sql = "SELECT * FROM products WHERE product_id = ?";
+
+  db.query(sql, productId, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    } else if (result.length === 0) {
+      res.status(404).json({ error: 'Product not found' });
+    } else {
+      res.json(result[0]);
+    }
+  });
+})
+
+
+app.post('/products', (req, res) => {
+  const sql = "INSERT INTO products SET ?";
+  const newProduct = [req.body];
+  db.query(sql, newProduct, (err, result) => {
+    if (err) {
+      console.log(err)
+      return;
+    } else {
+      newProduct.id = result.insertId;
+      res.status(201).json(newProduct);
+    }
+  })
+})
+
+app.put('/products', (req, res) => {
+  const productId = req.query.product_id;
+  const updatedProduct = req.body;
+
+  const sql = "UPDATE products SET ? WHERE product_id = ?";
+
+  db.query(sql, [updatedProduct, productId], (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    } else if (result.affectedRows === 0) {
+      res.status(404).json({ error: 'Product not found' });
+    } else {
+      res.json({ message: 'Product updated successfully' });
+    }
+  });
+})
+
+app.delete('/products', (req, res) => {
+  const productId = [req.query.product_id];
+  const sql = "DELETE FROM products WHERE product_id = ?";
+  db.query(sql, productId, (err, result) => {
+    if (err) {
+      console.log(err);
+      return;
+    } else if (result.affectedRows === 0) {
+      res.status(404).json({ err: 'Product not found!' });
+    } else {
+      res.json({ message: 'Product deleted!' });
+    }
+  });
 })
 
 app.listen(3344, () => {
